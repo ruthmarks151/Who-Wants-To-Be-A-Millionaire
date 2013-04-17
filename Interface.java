693429fe9255dcb8f42f4a343fc68c345ae0f9d7
Ron O. <ronald33838@yahoo.ca>
2013-04-17 08:50:51 -0400

    Auto indented Interface.java

--- Interface.java
+++ Interface.java
@@ -1,12 +1,15 @@
-class Interface{
-    public static void main(String args[]){
-QuestionDeck qd=new QuestionDeck();
-qd.fill();
-Question q1=qd.get();
-q1.display();
-      //QuestionLoader ql=new QuestionLoader();
-      //ql.pukeFile();
-      //System.out.println((int)'\n'+" ");
+class Interface
+{
+    public static void main (String args[])
+    {
+
+	QuestionDeck qd = new QuestionDeck ();
+	qd.fill ();
+	Question q1 = qd.get ();
+	q1.display ();
+
+	//QuestionLoader ql=new QuestionLoader();
+	//ql.pukeFile();
+	//System.out.println((int)'\n'+" ");
     }
-    
 }
